700717da1c7c7d9e6d753889cf33c50938af682c
Heuristic Services <admin@heuristicservices.com>
2019-09-14 09:21:38 +0100

    Initial commit

---
+++ src/main/java/com/heuristicservices/springboothibernatepostgresuuids/SpringBootHibernatePostgresUuidsApplication.java
@@ -0,0 +1,13 @@
+package com.heuristicservices.springboothibernatepostgresuuids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHibernatePostgresUuidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHibernatePostgresUuidsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heuristicservices/springboothibernatepostgresuuids/SpringBootHibernatePostgresUuidsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.heuristicservices.springboothibernatepostgresuuids;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootHibernatePostgresUuidsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
